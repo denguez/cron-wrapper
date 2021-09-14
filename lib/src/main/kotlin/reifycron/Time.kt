7e3f5a3544028c349590c4f4f6705b8201b21764
jc <jc@pop-os.localdomain>
2021-09-14 14:54:07 +0200

    fix comma

--- lib/src/main/kotlin/reifycron/Time.kt
+++ lib/src/main/kotlin/reifycron/Time.kt
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 class HourTime(val hour: Int, val minute: Int)
 
 enum class Day {
-    MON, TUE, WED, THU, FRI, SAT, SUN
+    MON, TUE, WED, THU, FRI, SAT, SUN;
     companion object {
         fun format(days: Set<Day>) = days.joinToString(",") { (it.ordinal + 1).toString() }
     }
